42632cbb5b1933c5d71f8f2e979f54656c9fa3e1
adnan ali <adnanali11875@gmail.com>
2025-02-16 17:52:05 +0530

    cart routes changed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/auth/(.*)", // Adjust this path based on where your auth routes are
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin-allow-popups" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" }, // Optional
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
